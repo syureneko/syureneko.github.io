a2324fdcb2543a78a1c2e30b918b1aa1f3bd4031
rei <syureneko.ngs@gmail.con>
2021-09-05 01:02:37 +0900

    お問い合わせのデザインを変更

---
+++ javaScript/checkInquiry.js
@@ -0,0 +1,9 @@
+﻿function checkSubmit(){
+  const name = document.getElementById("characterNameForm");
+  const inquiry = document.getElementById("inquiryForm");
+  if (name && name.value && inquiry && inquiry.value) {
+  	document.myForm.submit();
+    document.getElementById('formWrapper').style.display = 'none';
+    document.getElementById('thxMessage').style.display = 'block';
+  }
+}
